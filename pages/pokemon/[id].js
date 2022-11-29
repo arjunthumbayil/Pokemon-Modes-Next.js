05a9ec04a4dbfa5eed1e4fdd6cdf90b1ae508d43
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-29 22:31:44 +0530

    details page css added

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,8 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/Details.module.css";
 import { useRouter } from "next/router";
 
 export default function Details() {
   const {
     query: { id },
   } = useRouter();
-  return <div>hello - {id}</div>;
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    async function getPokemonDetails() {
+      const response = await fetch(
+        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
+      );
+      const data = await response.json();
+      //   console.log("data = " + data);
+      setPokemon(data);
+    }
+    if (id) {
+      getPokemonDetails();
+    }
+  }, [id]);
+
+  if (!pokemon) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Pokemon List</title>
+      </Head>
+    </div>
+  );
 }
