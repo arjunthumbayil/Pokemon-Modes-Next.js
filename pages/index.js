b3fffce2eb55d9e90d024c5ff649be7c92954e35
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-29 22:00:35 +0530

    image rendering errors resolved

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    async function getPokemon() {
+    async function getPokemons() {
       const response = await fetch(
         "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
       );
       const data = await response.json();
       console.log("data = " + data);
-      setPokemon(data);
+      setPokemons(data);
     }
-    getPokemon();
+    getPokemons();
   }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>Pokemon List</title>
       </Head>
+      <div className={styles.grid}>
+        {pokemons.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+            <Link href={`/pokemon/${pokemon.id}`} legacyBehavior>
+              <a>
+                {/* <Image alt={pokemon.name} /> */}
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                />
+                <h3>{pokemon.name}</h3>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
