e80fd81be9c6a27a61606121d55410ab496b99d5
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-30 06:44:46 +0530

    refactored to static site generation (ssg)

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await fetch(
     "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
   );
@@ -30,7 +30,6 @@ export default function Home({ pokemons }) {
           <div className={styles.card} key={pokemon.id}>
             <Link href={`/pokemon/${pokemon.id}`} legacyBehavior>
               <a>
-                {/* <Image alt={pokemon.name} /> */}
                 <img
                   src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
                   alt={pokemon.name}
